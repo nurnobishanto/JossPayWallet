463b9d4fb9e760efb4f9c9d3a543b750b6f7a599
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-11-12 16:38:39 +0600

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'JOSS Pay Wallet',
+    'title' => env('APP_NAME'),
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>Joss</b>Pay',
+    'logo' => env('APP_NAME'),
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
@@ -253,7 +253,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => 'admin',
     'logout_url' => 'admin/logout',
     'login_url' => 'admin/login',
     'register_url' => 'admin/register',

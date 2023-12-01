997e20eb49e426564443d1b17a4309f623057827
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-12-01 22:59:13 +0600

    update link

--- routes/web.php
+++ routes/web.php
@@ -24,25 +24,8 @@ Route::post('/success',[\App\Http\Controllers\PaymentController::class,'success'
 Route::post('/fail',[\App\Http\Controllers\PaymentController::class,'fail'])->name('fail');
 Route::get('/cancel',[\App\Http\Controllers\PaymentController::class,'cancel'])->name('cancel');
 Route::get('qr/{store}/default-payment',[\App\Http\Controllers\PLController::class,'default_payment_link_qr_code']);
-Route::get('{store}/d-pay',[\App\Http\Controllers\PLController::class,'default_payment_link']);
+Route::get('{store}/default-payment',[\App\Http\Controllers\PLController::class,'default_payment_link']);
 Route::get('qr/{store}/d-pay',[\App\Http\Controllers\PLController::class,'default_payment_link_qr_code'])->name('store.default_payment_link_qr_code');
-Route::get('{store}/default-payment',[\App\Http\Controllers\PLController::class,'default_payment_link'])->name('store.default_payment_link');
+Route::get('{store}/d-pay',[\App\Http\Controllers\PLController::class,'default_payment_link'])->name('store.default_payment_link');
 Route::post('payment-status',[\App\Http\Controllers\PLController::class,'payment_status'])->name('payment_status');
-Route::get('/qr', function () {
-    $qrCodes = [];
-    $myStore = Store::find(2);
-    $link = route('store.default_payment_link',['store' => $myStore->store_id]);
-    $qrCodes['qrCodeImage'] = null;
-    $qrCodes['qrCode'] = null;
-    $qrCodes['title'] = 'Default Payment Link';
-    $qrCodes['store'] = $myStore;
-    $qrCodes['link'] = $link;
-    if ($myStore->business_logo){
-        $image = '/public/uploads/'.$myStore->business_logo;
-        $qrCodes['qrCodeImage'] = QrCode::size(350)->format('png')->merge($image, .2)->generate($link);
 
-    }else{
-        $qrCodes['qrCode'] = QrCode::size(350)->generate($link);
-    }
-    return view('qr', $qrCodes);
-});

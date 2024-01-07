72f5d4c7681a5f22ab7bdaa78c5d43163d0e8ba0
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2024-01-07 18:46:25 +0600

    Update loading.blade.php

--- resources/views/payment/loading.blade.php
+++ resources/views/payment/loading.blade.php
@@ -67,14 +67,14 @@
         </div>
     </div>
 </div>
-{!! $formHtml !!}
+
 <!-- Bootstrap JS and jQuery -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 <script>
-    document.getElementById('redirectForm').submit();
+  
     // Show spinner on page load
     $(document).ready(function () {
         $('#spinner').show();
